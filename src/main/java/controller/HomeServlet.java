ddd3cfc75a92a2b144945b039d0c1c57752727d0
Mehdi <elmehdiworkemail@gmail.com>
2024-10-02 11:37:28 +0000

    dashboard main page done

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -12,6 +12,6 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("views/home.jsp").forward(request, response);
+        request.getRequestDispatcher("views/dashboard/home.jsp").forward(request, response);
     }
 }
