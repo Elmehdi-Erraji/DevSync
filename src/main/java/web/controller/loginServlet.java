6701e17b6bca80de0e9e62500affd2fedca0fe4d
Mehdi <elmehdiworkemail@gmail.com>
2024-10-16 11:22:08 +0000

    working on the tests

--- src/main/java/web/controller/loginServlet.java
+++ src/main/java/web/controller/loginServlet.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.Set;
 
-@WebServlet("/login")
+@WebServlet("/")
 public class loginServlet  extends HttpServlet {
 
     private UserService userService = new UserService();
