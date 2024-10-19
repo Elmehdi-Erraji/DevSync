ecc18614508211b4bb59cd701a1d87616017193c
Mehdi <elmehdiworkemail@gmail.com>
2024-10-19 20:19:21 +0000

    adding tests on the tag service

--- src/test/java/service/TagServiceTest.java
+++ src/test/java/service/TagServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 import repository.TagRepositoryInterface;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class TagServiceTest {
@@ -29,13 +30,78 @@ public class TagServiceTest {
     public void testInsertTag_ValidTag() {
         Tag tag = new Tag();
         tag.setName("tag1");
+        when(tagRepository.save(tag)).thenReturn(tag);
+        Tag result = tagService.insertTag(tag);
+        assertEquals(tag, result);
+        verify(tagRepository, times(1)).save(tag);
+    }
+
+    @Test
+    public void testInsertTag_EmptyName() {
+        Tag tag = new Tag();
+        tag.setName("");
+
+        TagException exception = assertThrows(
+                TagException.class,
+                () -> tagService.insertTag(tag)
+        );
+
+        assertEquals("Tag name cannot be null or empty.", exception.getMessage());
+        verify(tagRepository, never()).save(any(Tag.class));
+    }
+
+    @Test
+    public void testInsertTag_LongName() {
+        Tag tag = new Tag();
+        tag.setName("a".repeat(51));
+
+        TagException exception = assertThrows(
+                TagException.class,
+                () -> tagService.insertTag(tag)
+        );
+        assertEquals("Tag name cannot exceed 50 characters.", exception.getMessage());
+        verify(tagRepository, never()).save(any(Tag.class));
+    }
+
+    @Test
+    public void testUpsertTag_ValidTag() {
+        Tag tag = new Tag();
+        tag.setId(1L);
+        tag.setName("tag1");
 
         when(tagRepository.save(tag)).thenReturn(tag);
 
         Tag result = tagService.insertTag(tag);
-
         assertEquals(tag, result);
         verify(tagRepository, times(1)).save(tag);
+    }
+
+    @Test
+    public void testUpdateTag_NullId(){
+        Tag tag = new Tag();
+        tag.setName("tag1");
+
+        TagException exception = assertThrows(
+                TagException.class,
+                () -> tagService.updateTag(tag)
+        );
+
+        assertEquals("Tag ID cannot be null when updating a tag.", exception.getMessage());
+        verify(tagRepository, never()).save(any(Tag.class));
+    }
+
+    @Test
+    public void testUpdateTag_EmptyName(){
+        Tag tag = new Tag();
+        tag.setId(1L);
+        tag.setName("");
+
+        TagException exception = assertThrows(
+                TagException.class,
+                () -> tagService.updateTag(tag)
+        );
+        assertEquals("Tag name cannot be null or empty.", exception.getMessage());
+        verify(tagRepository, never()).save(any(Tag.class));
     }
 
 }
